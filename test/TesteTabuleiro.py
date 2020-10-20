6d3bfd5fb0b216a633a5de84898d8b8e5cc26f67
Jam Ajna Soares <ajnasoares@hotmail.com>
2020-10-19 21:19:19 -0300

    cleancode nome do arquivo + teste criar e mover

--- test/TesteTabuleiro.py
+++ test/TesteTabuleiro.py
@@ -3,6 +3,17 @@ from src.Tabuleiro import *
 
 class TestTabuleiro(unittest.TestCase):
 
+    def testeCriarTabuleiro(self):
+        if (criarTabuleiro() == 1):
+            print("Tabuleiro criado com sucesso!\n")
+        
+    def testeMoverTabuleiro(self, n): # teste a função na n+1-ésima posição
+        i = 0
+        while (i < n):
+            if (moverTabuleiro() == 1): # no futuro 'n' pode ser útil para determinar número de rodadas/turnos da partida
+                    print("Tabuleiro girado com sucesso!\n")
+            i+=1
+
     def testeDefinirStatusCasaPassavel(self):
         print("Caso de teste 1 - casas passáveis do tabuleiro")
         retornoEsperado = defineStatusCasa(10)
