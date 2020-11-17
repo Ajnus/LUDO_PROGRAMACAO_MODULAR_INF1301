e9db092b40da375241dfa4bf86e60ac056037de4
Jam Ajna Soares <ajnasoares@hotmail.com>
2020-11-16 23:47:03 -0300

    root:root

--- db/base.py
+++ db/base.py
@@ -1,8 +1,8 @@
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import create_engine
 #                                                username:senha@host:port/database
-db_engine = create_engine('postgresql+psycopg2://postgres:123@localhost:5432/LUDO')
+db_engine = create_engine('postgresql+psycopg2://root:root@localhost:5432/INF1301')
 Base = declarative_base()
 Base.metadata.bind = db_engine
 
-peoesCoresDisponiveis= ['Amarelo', 'Azul', 'Verde', 'Vermelho']
+peoesCoresDisponiveis = ['Amarelo', 'Azul', 'Verde', 'Vermelho']
