179c0a55981a2e8d9cb18627c59e54dadc91b868
Jam Ajna Soares <ajnasoares@hotmail.com>
2020-11-17 00:04:09 -0300

    PEP8

--- test/teste_excecoes.py
+++ test/teste_excecoes.py
@@ -1,54 +1,54 @@
 from src.excecoes import *
 import unittest
 
-class TestExcecoes(unittest.TestCase):
 
-    def testValidaNumeroInteiroCorretamente(self):
+class TesteExcecoes(unittest.TestCase):
+
+    def testeValidaNumeroInteiroCorretamente(self):
         excecao = InputError()
         resultado = excecao.validaNumeroInteiro('3')
         self.assertEqual(0, resultado)
 
-    def testValidaNumeroInteiroPassandoChar(self):
+    def testeValidaNumeroInteiroPassandoChar(self):
         excecao = InputError()
         resultado = excecao.validaNumeroInteiro('k')
         self.assertEqual(2, resultado)
 
-    def testValidaNumeroInteiroPassandoFloat(self):
+    def testeValidaNumeroInteiroPassandoFloat(self):
         excecao = InputError()
         resultado = excecao.validaNumeroInteiro('2.3')
         self.assertEqual(2, resultado)
 
-
-    def testValidaIntervaloCorretamente(self):
+    def testeValidaIntervaloCorretamente(self):
         excecao = InputError()
         resultado = excecao.validaIntervalo(2, 1, 4)
         self.assertEqual(0, resultado)
 
-    def testValidaIntervaloAcimaDoIntervalo(self):
+    def testeValidaIntervaloAcimaDoIntervalo(self):
         excecao = InputError()
         resultado = excecao.validaIntervalo(5, 1, 4)
         self.assertEqual(1, resultado)
 
-    def testValidaIntervaloAbaixoDoIntervalo(self):
+    def testeValidaIntervaloAbaixoDoIntervalo(self):
         excecao = InputError()
         resultado = excecao.validaIntervalo(0, 1, 4)
         self.assertEqual(1, resultado)
 
-
-    def testValidaNomeCorretamente(self):
+    def testeValidaNomeCorretamente(self):
         excecao = InputError()
         resultado = excecao.validaNome('Luan')
         self.assertEqual(0, resultado)
 
-    def testValidaNomeMaiorQueLimiteDeCaracteres(self):
+    def testeValidaNomeMaiorQueLimiteDeCaracteres(self):
         excecao = InputError()
         resultado = excecao.validaNome('Luan Aguiar dos Santos Ferreira')
         self.assertEqual(1, resultado)
 
-    def testValidaNomeEmBranco(self):
+    def testeValidaNomeEmBranco(self):
         excecao = InputError()
         resultado = excecao.validaNome('')
         self.assertEqual(1, resultado)
+
 
 if __name__ == '__main__':
     unittest.main()
