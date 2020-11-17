56fa3cac43fd0e9d7b42f272aaa107601c71aec4
Jam Ajna Soares <ajnasoares@hotmail.com>
2020-11-16 23:09:19 -0300

    Update base.py

--- db/base.py
+++ db/base.py
@@ -1,7 +1,7 @@
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import create_engine
 #                                                username:senha@host:port/database
-db_engine = create_engine('postgresql+psycopg2://postgres:123@localhost:5432/INF1301')
+db_engine = create_engine('postgresql+psycopg2://root:root@localhost:5432/INF1301')
 Base = declarative_base()
 Base.metadata.bind = db_engine
 
